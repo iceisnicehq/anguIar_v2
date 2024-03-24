122074ba0434356044a639a45064aaa97044af48
Ice-hq <66420569+Ice-hq@users.noreply.github.com>
2024-03-24 21:54:56 +0000

    table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,60 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
+import {MatTable} from '@angular/material/table';
+import {MatTableModule} from '@angular/material/table';
 
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'lab1 brrrr';
-  death = "sweet";
-  public fullName: string = "it's my life"; 
+  name: string = '';
+  position: number = 0;
+  weight: number = 0;
+  symbol: string = '';
+  newrow = [];
+  title = 'lab2222';
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = [...ELEMENT_DATA];
+
+  @ViewChild(MatTable) table?: MatTable<PeriodicElement>;
+
+  addData() {
+    const randomElementIndex = Math.floor(Math.random() * ELEMENT_DATA.length);
+    this.dataSource.push(ELEMENT_DATA[randomElementIndex]);
+    this.table?.renderRows();
+  }
+
+  removeData() {
+    this.dataSource.pop();
+    this.table?.renderRows();
+  // addHandler(){
+  //   this.newrow.push({
+  //         elpos:this.position,
+  //         elname:this.name,
+  //         elweight:this.weight,
+  //         elsymbol:this.symbol,
+  //   })
+  //   }
+  }
 }
- 
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DialogComponent } from './dialog/dialog.component';
+import {MatTableModule} from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
+import {FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DialogComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatTableModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
