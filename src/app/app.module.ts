9c4210766bfdfb3f144452303b52720cd8656cae
Ice-hq <iceisnicehq@gmail.com>
2024-04-06 21:08:00 +0300

    mr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DialogComponent } from './dialog/dialog.component';
+import { DialogComponent } from './entities/components/dialog/dialog.component';
 import {MatTableModule} from '@angular/material/table';
 import {MatButtonModule} from '@angular/material/button';
 import {FormsModule } from '@angular/forms';
